e75d738e1eae622e5c005be817b9e72673d36e42
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-04-18 08:40:36 -0500

    linked index.html with single-repo.html

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,54 +1,92 @@
-var issuesContainerEl = document.querySelector("#issues-container");
+var repoNameEl = document.querySelector("#repo-name");
+var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoContainerEl = document.querySelector("#repo-name");
 
-var displayIssues = function (issues) {
-  if (issues.length === 0) {
-    alert("There are no open issues");
-    return;
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+  } else {
+    document.location.replace("./index.html");
   }
-  for (var i = 0; i < issues.length; i++) {
-    var issuesEl = document.createElement("a");
-    issuesEl.classList =
-      "list-item flex-row justify-space-between align-center";
-    issuesEl.setAttribute("href", issues[i].html_url);
-    issuesEl.setAttribute("target", "_blank");
-    var titleEl = document.createElement("span");
-    titleEl.textContent = issues[i].title;
-    issuesEl.appendChild(titleEl);
-    var typeEl = document.createElement("span");
-    if (issues[i].pull_request) {
-      typeEl.textContent = "(Pull request)";
-    } else {
-      typeEl.textContent = "(Issue)";
-    }
-    issuesEl.appendChild(typeEl);
-    issuesContainerEl.appendChild(issuesEl);
-  }
-};
-
-var displayWarning = function (repo) {
-  limitWarningEl.textContent = "To see more than 30 issues, visit ";
-  var linkEl = document.createElement("a");
-  linkEl.textContent = "See more issues on GitHub.com";
-  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-  linkEl.setAttribute("target", "_blank");
-  limitWarningEl.appendChild(linkEl);
 };
 
 var getRepoIssues = function (repo) {
+  // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  // make a get request to url
   fetch(apiUrl).then(function (response) {
+    // request was successful
     if (response.ok) {
       response.json().then(function (data) {
         displayIssues(data);
+
+        // check if api has paginated issues
         if (response.headers.get("Link")) {
           displayWarning(repo);
         }
       });
     } else {
-      alert("There was a problem with the request");
+      document.location.replace("./index.html");
     }
   });
 };
 
-getRepoIssues("facebook/react");
+var displayIssues = function (issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  // loop over given issues
+  for (var i = 0; i < issues.length; i++) {
+    // create a link element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    // append to container
+    issueEl.appendChild(titleEl);
+
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or a pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+
+    // append to the dom
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+var displayWarning = function (repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  // create link element
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
